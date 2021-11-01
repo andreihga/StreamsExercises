9f677bfed3e20dd3ce74ec9e5c325c6a1b541878
Andrei Adrian Hogea <Andrei.Hogea@endava.com>
2021-11-01 12:48:49 +0200

    refactored isPrime

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
         List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
-        IsPrime isPrime = new IsPrime();
-        printNumbersByCriteria(integerList, isPrime);
+        printNumbersByCriteria(integerList, number -> {
+            IntPredicate intPredicate = index -> number % index == 0;
+            return number > 1 && IntStream.range(2, number).noneMatch(intPredicate);
+        });
     }
 
     public static void printNumbersByCriteria(List<Integer> numbers, Predicate<Integer> predicate) {
