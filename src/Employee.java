7ba95f8e31e7956e97f1341ea57cc4f06df27653
Hogea Andrei <andreihga@gmail.com>
2021-11-02 13:23:21 +0200

    - raw implementation of groupEmployeesByJobTitle

---
+++ src/Employee.java
@@ -0,0 +1,34 @@
+public class Employee {
+    String name;
+    String jobTitle;
+
+
+    public Employee(String name, String jobTitle) {
+        this.name = name;
+        this.jobTitle = jobTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,61 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
 
+        Employee employee1 = new Employee("Andrei", "Programator");
+        Employee employee2 = new Employee("Adelina", "Programator");
+        Employee employee3 = new Employee("Madalin", "Inginer");
+        Employee employee4 = new Employee("Andreea", "Inginer");
+        Employee employee5 = new Employee("Robi", "Asistent");
+        Employee employee6 = new Employee("Rita", "Doctor");
+        Employee employee7 = new Employee("Evelynn", "Copil");
+        Employee employee8 = new Employee("Cosmin", "Asistent");
+        Employee employee9 = new Employee("Adrian", "Inginer");
+
+        List<Employee> employeeList = new ArrayList<>() {{
+            add(employee1);
+            add(employee2);
+            add(employee3);
+            add(employee4);
+            add(employee5);
+            add(employee6);
+            add(employee7);
+            add(employee8);
+            add(employee9);
+        }};
+
+        Map<String, List<Employee>> employeesSortedByJobTitle = groupEmployeesByJobTitle(employeeList);
+        for (String jobTitle : employeesSortedByJobTitle.keySet()) {
+
+            System.out.println(jobTitle);
+            System.out.println(employeesSortedByJobTitle.get(jobTitle));
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+        }
+
     }
+
+    public static Map<String, List<Employee>> groupEmployeesByJobTitle(List<Employee> employeeList) {
+
+        Map<String, List<Employee>> groupedEmployees = new HashMap<>();
+        for (Employee employee : employeeList) {
+            if (groupedEmployees.containsKey(employee.jobTitle)) {
+                List<Employee> subListOfEmployees = groupedEmployees.get(employee.jobTitle);
+                subListOfEmployees.add(employee);
+                groupedEmployees.put(employee.jobTitle, subListOfEmployees);
+            } else {
+                groupedEmployees.put(employee.jobTitle, new ArrayList<>() {{
+                    add(employee);
+                }});
+            }
+        }
+        return groupedEmployees;
+    }
+
 }
